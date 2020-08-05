ed8791a19a5d474a250ad5c2b28c9356742e471e
Mike Koro <mikekoro86@gmail>
2020-08-04 22:22:39 -0400

    added empty state

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,14 @@ import GistsList from './../GistsList';
 import LoadingState from './../Misc/LoadingState';
 import SearchForm from './../SearchForm';
 import ErrorState from './../Misc/ErrorState';
+import EmptyState from './../Misc/EmptyState';
 
 const Home = () => {
 
     const [gists, setGists] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [empty, setEmpty] = useState(false);
 
     /**
      * Submits the query to the API and performs basic DOM manipulation
@@ -20,6 +22,7 @@ const Home = () => {
         setError(null); // reset error message just in case
         setGists([]); // cleanup the list of gists just in case
         setLoading(true); // enable loading indicator
+        setEmpty(false); // reset empty state
 
         const [err, response] = await getGistsByUsername(query); // call the API
 
@@ -29,7 +32,9 @@ const Home = () => {
             setError(err);
             return;
         }
-
+        if(response.data.length === 0) {
+            setEmpty(true); // enable empty state
+        }
         setGists(response.data); // populate the list of Gists
         setLoading(false); // remove loading indicator
 
@@ -53,7 +58,9 @@ const Home = () => {
                     <ErrorState 
                         error={error}
                     />
-
+                    <EmptyState
+                        is_empty={empty}
+                    />
                 </div>
             </div>
         </div>
---
+++ src/components/Misc/EmptyState.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Empty state UI
+ * @param {boolean} is_empty Flag to enable or disable empty state 
+ */
+
+const EmptyState = ({is_empty}) => {
+    if(!is_empty) return null;
+    return (
+        <div className="text-center">No results were returned:(</div>
+    )
+}
+
+export default EmptyState;
